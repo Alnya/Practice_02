94c65c928e216900ebe64c30c84843b21fe3e1ab
Alnya <momoseden@gmail.com>
2020-10-06 14:57:20 +0900

    update

--- send_dm.py
+++ send_dm.py
@@ -14,9 +14,13 @@ def type_enter():
     pyautogui.press("enter")
 
 
+def close_tab():
+    pyautogui.hotkey("ctrl", "w")
+
+
 def main(name, message):
     type_string(name)
-    time.sleep(1)
+    time.sleep(2)
     type_tab()
     time.sleep(0.1)
     type_enter()
@@ -33,3 +37,5 @@ def main(name, message):
     time.sleep(1)
     type_string(message)
     type_enter()
+    time.sleep(0.1)
+    close_tab()
